eb5af6483af064dc011b153b66a391003d86d89f
Irirna <burdina98@mail.ru>
2023-03-20 10:42:30 +0500

    Commit api

---
+++ src/main/java/ru/netology/api/Main.java
@@ -0,0 +1,22 @@
+package ru.netology.api;
+ import  ru.netology.api.Post;
+ import  ru.netology.api.FormDate;
+ import ru.netology.api.Main;
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post ();
+post.name = "Иван";
+post.passport = "4444 № 44444444";
+post.patronymic = "Иванович";
+post.phone = "+7 (999)-999-99-99";
+post.surname =    "Иванов";
+post.subscription = true;
+
+post.birthday = new FormDate();
+post.birthday.day = 13;
+
+post.birthday.month = 3;
+post.birthday.year = 2020;
+
+    }
+    }
